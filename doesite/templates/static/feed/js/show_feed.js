61d35eb621c5801dd0e581b73f5dc05d1a7e3d20
Vinicius Leite Mello <50744076+VLMello@users.noreply.github.com>
2023-06-24 19:45:42 -0300

    Botão de seguir

---
+++ doesite/templates/static/feed/js/show_feed.js
@@ -0,0 +1,32 @@
+function addListenerScroll() {
+    document.getElementById('scrollable-content').addEventListener('scroll', event => {
+        const {scrollHeight, scrollTop, clientHeight} = event.target;
+    
+        if (Math.abs(scrollHeight - clientHeight - scrollTop) < 1) {
+            loadPost();
+        }
+    });
+}
+
+function drawPost(owner, handle, text, likes, comments){
+    // Get the element
+    var elem = document.querySelector('#template-post');
+
+    // Create a copy of it
+    var clone = elem.cloneNode(true);
+
+    // Update the post
+    clone.id = 'drawnPost';
+    clone.querySelector('.name').innerText = owner;
+    clone.querySelector('.at').innerText = handle;
+    clone.querySelector('.description-text').innerText = text;
+    clone.querySelector('.fa-heart').innerText = likes;
+    clone.querySelector('.fa-comment').innerText = comments;
+
+    // Inject it into the DOM
+    document.getElementById("scrollable-content").appendChild(clone);
+}
+
+function loadPost(){
+    drawPost("pessoa", "@pes", "carregado", "12345", "985");
+}  
