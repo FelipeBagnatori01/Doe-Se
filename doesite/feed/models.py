c88363cb3479186b46f07713cef4f1c66ab87419
Felipe Bagnatori Rolim <90479524+FelipeBagnatori01@users.noreply.github.com>
2023-07-13 23:34:08 -0300

    user

--- doesite/feed/models.py
+++ doesite/feed/models.py
@@ -1,5 +1,5 @@
 from django.db import models
-from django.contrib.auth.models import User
+from django.contrib.auth import get_user_model
 
 
 class Institute(models.Model):
@@ -19,14 +19,15 @@ class Institute(models.Model):
 
 class Follows(models.Model):
     follow = models.ForeignKey(Institute, on_delete=models.CASCADE)
-    followers = models.ForeignKey(User, on_delete=models.CASCADE)
+    followers = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
+    
 class Post(models.Model):
     id = models.AutoField(primary_key=True)
     text = models.CharField(max_length=50)
     image = models.ImageField()
     likes = models.IntegerField()
-    User = models.ForeignKey(User, on_delete=models.CASCADE)
+    user = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
 
     def __str__(self) -> str:
         return self.id
