03b7a99b482d13fbd7c6edf2c121519bd85e6ad4
Felipe Bagnatori Rolim <90479524+FelipeBagnatori01@users.noreply.github.com>
2023-07-16 19:18:24 -0300

    Admin site

--- doesite/feed/admin.py
+++ doesite/feed/admin.py
@@ -1,10 +1,21 @@
 from django.contrib import admin
-
+from django.contrib.auth.models import Group, User
 # Register your models here.
 from .models import *
 
+admin.site.unregister(Group)
 admin.site.register(Profile)
 admin.site.register(Post)
 
 class ProfileInLine(admin.StackedInline):
     model = Profile
+
+class UserAdmin(admin.ModelAdmin):
+    model = User
+    fields = ["username"]
+    inLines = [ProfileInLine]
+    
+admin.site.unregister(User)
+admin.site.register(User, UserAdmin)
+#admin.site.register(Profile)
+
