45d359eed91ac597077a3e8660ee982dbefcded1
Felipe Bagnatori Rolim <90479524+FelipeBagnatori01@users.noreply.github.com>
2023-07-17 03:15:18 -0300

    Update models.py

--- doesite/feed/models.py
+++ doesite/feed/models.py
@@ -6,27 +6,6 @@ from django.db.models.signals import post_save
 from django.utils import timezone
 import json
 
-'''
-class Institute(models.Model):
-    id = models.AutoField(primary_key=True)
-    name = models.CharField(max_length=50)
-    email = models.CharField(max_length=50)
-    phone = models.IntegerField()
-    cep = models.IntegerField()
-    uf = models.CharField(max_length=50)
-    adress = models.CharField(max_length=50)
-    number = models.IntegerField()
-    psw = models.CharField(max_length=50)
-    description = models.CharField(max_length=500)
-    
-    def __str__(self) -> str:
-        return self.name
-
-class Follows(models.Model):
-    follow = models.ForeignKey(Institute, on_delete=models.CASCADE)
-    followers = models.ForeignKey(get_user_model(), on_delete=models.CASCADE)
-
-'''
 
 class Profile(models.Model):
     user = models.OneToOneField(get_user_model(), on_delete=models.CASCADE)
